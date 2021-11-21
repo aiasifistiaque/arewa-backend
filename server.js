6cb0d84aa51b1586661c22f17686343a640af303
asifistiaque <asifistiaque.ai@gmail.com>
2021-11-22 02:40:01 +0600

    asds

--- server.js
+++ server.js
@@ -91,13 +91,13 @@ app.use(function (req, res, next) {
 });
 
 app.enable('trust proxy');
-app.use((req, res, next) => {
-	if (req.secure) {
-		next();
-	} else {
-		res.redirect('https://' + req.headers.host + req.url);
-	}
-});
+// app.use((req, res, next) => {
+// 	if (req.secure) {
+// 		next();
+// 	} else {
+// 		res.redirect('https://' + req.headers.host + req.url);
+// 	}
+// });
 
 //routes
 app.use('/api/auth', authRoute);
