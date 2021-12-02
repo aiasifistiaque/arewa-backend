c310929e6ce349d5e91f9bd67021f6ea87037b05
asifistiaque <asifistiaque.ai@gmail.com>
2021-12-02 19:24:37 +0600

    asd

--- controllers/info/bookInfoController.js
+++ controllers/info/bookInfoController.js
@@ -1,5 +1,6 @@
 import Book from '../../models/bookModel.js';
 import asyncHandler from 'express-async-handler';
+import * as lib from '../../lib/lib.js';
 
 const bookInfoController = asyncHandler(async (req, res) => {
 	try {
@@ -13,9 +14,19 @@ const bookInfoController = asyncHandler(async (req, res) => {
 			]);
 
 		if (!book)
-			return res.status(404).json({ status: 'error', message: error.message });
+			return res
+				.status(404)
+				.json({ status: 'error', message: 'book not found' });
 
-		res.status(200).json({ status: 'success', doc: book });
+		const doc = {
+			_id: book._id,
+			title: book.title,
+			author: book.author,
+			image: book.image,
+			description: lib.stripHtml(book.description),
+		};
+
+		res.status(200).json({ status: 'success', doc });
 	} catch (error) {
 		res.status(404).json({ status: 'error', message: error.message });
 	}
--- routes/infoRoute.js
+++ routes/infoRoute.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import bookInfoController from '../controllers/info/bookInfoController.js';
+import chapterInfoController from '../controllers/info/chapterInfoController.js';
 
 /**Swagger doc
  * completed
@@ -12,5 +13,6 @@ import bookInfoController from '../controllers/info/bookInfoController.js';
 const router = express.Router();
 
 router.get('/book/:id', bookInfoController);
+router.get('/chapter/:id', chapterInfoController);
 
 export default router;
