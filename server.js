70a4fe13d87efd766ce2129e96d1bff10725ac37
Umar Muhammad <arewabookspublishers@gmail.com>
2022-04-19 17:59:55 +0100

    cors

--- server.js
+++ server.js
@@ -104,16 +104,16 @@ const swaggerDocs = swaggerJsDoc(swaggerOptions);
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 app.use(cors());
-
-app.use(function (req, res, next) {
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-	res.setHeader(
-		'Access-Control-Allow-Headers',
-		'Origin, X-Requested-With, Content-Type, Accept'
-	);
-	next();
-});
+app.use(cors({credentials: true,optionsSuccessStatus: 200, origin: '*'}));
+// app.use(function (req, res, next) {
+// 	res.setHeader('Access-Control-Allow-Origin', '*');
+// 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+// 	res.setHeader(
+// 		'Access-Control-Allow-Headers',
+// 		'Origin, X-Requested-With, Content-Type, Accept'
+// 	);
+// 	next();
+// });
 
 //routes
 app.use('/api/auth', authRoute);
